d4788e7bd93a2360f92560cad94b3fa27074c6b2
SHOMAN <mohammedshoman9@gmail.com>
2020-10-18 01:23:45 +0300

    fix errors

---
+++ docs/precache-manifest.67da14f5abb909fa1ade146aed457389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80681ffac643ab72b178bb4f40acc3c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e26726d07a18c3d66a91",
+    "url": "./static/css/main.b645a9bc.chunk.css"
+  },
+  {
+    "revision": "351a612053b546ae2f9d",
+    "url": "./static/js/2.25cf84e5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.25cf84e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e26726d07a18c3d66a91",
+    "url": "./static/js/main.4228c82e.chunk.js"
+  },
+  {
+    "revision": "b793c38fc953fa14b65d",
+    "url": "./static/js/runtime-main.208b55f4.js"
+  }
+]);
